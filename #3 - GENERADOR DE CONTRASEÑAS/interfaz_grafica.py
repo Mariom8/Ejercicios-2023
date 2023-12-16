4e7afe86ce80b18b17f1b2a479e29391dd41df89
Mario Gonzalez <gonzalez.mario16@gmail.com>
2023-12-16 19:44:10 +0100

    mario
    v0.1

--- #3 - GENERADOR DE CONTRASEÑAS/interfaz_grafica.py
+++ #3 - GENERADOR DE CONTRASEÑAS/interfaz_grafica.py
@@ -1,13 +1,22 @@
 # interfaz_grafica.py
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import CENTER, ttk, messagebox, font
+from turtle import right
 from logica_contraseñas import LogicaContraseñas
 
 class GeneradorContraseñas:
     def __init__(self):
         self.ventana = tk.Tk()
         self.ventana.title("Generador de Contraseñas")
-
+        self.ventana.geometry("500x400")
+        
+        
+        
+        
+        # self.ventana.columnconfigure(0, weight=0)
+        # self.ventana.columnconfigure(1, weight=0)
+        # self.ventana.columnconfigure(2, weight=0)
+        
         self.longitud_label = ttk.Label(self.ventana, text="Longitud de la Contraseña:")
         self.longitud_entry = ttk.Entry(self.ventana)
         self.longitud_entry.insert(0, "12")
@@ -20,14 +29,25 @@ class GeneradorContraseñas:
         self.generar_button = ttk.Button(self.ventana, text="Generar Contraseña", command=self.generar_contraseña)
         self.contraseña_generada = tk.StringVar()
         self.contraseña_label = ttk.Label(self.ventana, textvariable=self.contraseña_generada)
+        self.titulo = ttk.Label(self.ventana, text="PASSGEN")
+        
+        # Configurar la columna central para expandirse
+        self.ventana.grid_columnconfigure(1, weight=1)
 
-        self.longitud_label.grid(row=0, column=0, pady=5, padx=5, sticky="w")
-        self.longitud_entry.grid(row=0, column=1, pady=5, padx=5)
-        self.mayus_checkbox.grid(row=1, column=0, pady=5, padx=5, sticky="w")
-        self.nums_checkbox.grid(row=2, column=0, pady=5, padx=5, sticky="w")
-        self.symbols_checkbox.grid(row=3, column=0, pady=5, padx=5, sticky="w")
-        self.generar_button.grid(row=4, column=0, columnspan=2, pady=10)
-        self.contraseña_label.grid(row=5, column=0, columnspan=2, pady=5)
+        # Alinear los elementos al centro
+        self.longitud_label.grid(row=0, column=0, sticky="e", padx=5, pady=5)
+        self.longitud_entry.grid(row=0, column=1, sticky="w", padx=5, pady=5)
+        self.mayus_checkbox.grid(row=1, column=0, columnspan=2, sticky="w", padx=5, pady=5)
+        self.nums_checkbox.grid(row=2, column=0, columnspan=2, sticky="w", padx=5, pady=5)
+        self.symbols_checkbox.grid(row=3, column=0, columnspan=2, sticky="w", padx=5, pady=5)
+        self.generar_button.grid(row=4, column=0, columnspan=2, sticky="w", padx=5, pady=5)
+        self.contraseña_label.grid(row=5, column=0, columnspan=2, sticky="w", padx=5, pady=5)
+
+
+
+        
+        
+
 
         self.logica = LogicaContraseñas()
 
